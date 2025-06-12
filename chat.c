7c70de46c54fcac5b3275c10dc87e56c30c88b11
Hug00x <111132271+Hug00x@users.noreply.github.com>
2025-06-12 01:56:53 +0100

    Update chat.c

--- chat.c
+++ chat.c
@@ -3,12 +3,14 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <sys/stat.h>
+#include <sys/types.h>
 
 void create_fifo(const char *name) {
+    mkdir("fifos", 0777);
     if (access(name, F_OK) == -1) {
         if (mkfifo(name, 0666) == -1) {
             perror("Erro ao criar FIFO");
             exit(EXIT_FAILURE);
-        }
-    }
+        }
+    }
 }
